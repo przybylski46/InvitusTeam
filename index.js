d676373c530ca1133dcf33874806625fe7b819fe
przybylski46 <przybylski4637@gmail.com>
2026-04-26 13:04:33 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -94,24 +94,6 @@ const commands = [
 ];
 
 // =====================
-// 🚀 REGISTRAR COMANDOS
-// =====================
-
-const rest = new REST({ version: '10' }).setToken(TOKEN);
-
-(async () => {
-  try {
-    await rest.put(
-      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-      { body: commands }
-    );
-    console.log("✅ Comandos registrados");
-  } catch (error) {
-    console.error(error);
-  }
-})();
-
-// =====================
 // 🤖 BOT LISTO
 // =====================
 
